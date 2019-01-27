8241e12afcbb503a2e1037471a9199c3a0b7aa7b
Rad Cirskis <nad2000@gmail.com>
2019-01-28 03:50:41 +0900

    reading up and parsing added

--- converter.go
+++ converter.go
@@ -2,11 +2,38 @@ package html2xlsx
 
 import (
 	"archive/zip"
+	"fmt"
+	"io"
 	"path/filepath"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/html"
 )
+
+func convert(r io.Reader) {
+
+	doc, err := html.Parse(r)
+	if err != nil {
+		log.WithError(err).Errorln("Failed to read the file content.")
+		return
+	}
+	// var table Table
+	// xml.Unmarshal(byteValue, &table)
+	// fmt.Printf("%#v\n\n\n", table)
+	var f func(n *html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "td" {
+			// Do something with n...
+			fmt.Printf("%#v\n", n)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+
+}
 
 // Convert opens zip file and goes throug all the .xls (with HTML content)
 // convrerts them into .xlsx and stores into a new output zipfile
@@ -24,16 +51,19 @@ func Convert(fileName, outputFilename string) {
 		if strings.HasPrefix(f.Name, "__MACOSX") || filepath.Ext(f.Name) != ".xls" {
 			continue
 		}
-		log.WithFields(log.Fields{"Filename": f.Name}).Infoln("Converting...")
-		// rc, err := f.Open()
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		log.WithField("Filename", f.Name).Infoln("Converting...")
+		rc, err := f.Open()
+		if err != nil {
+			log.WithError(err).Errorln("Failed to open file in the archive file.")
+			continue
+		}
+		convert(rc)
+
 		// _, err = io.CopyN(os.Stdout, rc, 68)
 		// if err != nil {
 		// 	log.Fatal(err)
 		// }
-		// rc.Close()
+		rc.Close()
 		// fmt.Println()
 	}
 }
