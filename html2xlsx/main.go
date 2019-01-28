86873e3ca4a8299966bbd8d3abe5a70333734ad7
Rad Cirskis <nad2000@gmail.com>
2019-01-28 19:34:02 +0900

    missing bit

---
+++ html2xlsx/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/nad2000/html2xlsx"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Errorln("Missing input filename.")
+		log.Infoln("USAGE: " + os.Args[0] + " <INPUT FILENAME> [<OUTPUT FILENAME>]")
+		os.Exit(-1)
+	}
+	filename := os.Args[1]
+	var outputFilename string
+	if len(os.Args) > 2 {
+		outputFilename = os.Args[2]
+	} else {
+		dir, fn := filepath.Split(filename)
+		ext := filepath.Ext(fn)
+		outputFilename = filepath.Join(dir,
+			strings.TrimSuffix(fn, ext)+"_OUTPUT"+ext)
+	}
+	html2xlsx.Convert(filename, outputFilename)
+	log.WithField("Output Filename", outputFilename).Infoln("The converted files stored.")
+}
