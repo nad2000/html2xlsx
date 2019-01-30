88e6fa13ab515e7467e7d35d6c158f6aa9b4014f
Rad Cirskis <nad2000@gmail.com>
2019-01-30 14:32:13 +0900

    default executable for Mac

--- html2xlsx/main.go
+++ html2xlsx/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 // VERSION of the module...
-const VERSION = "0.1.1"
+const VERSION = "0.1.2"
 
 func usage() {
 	log.Infoln("USAGE:\n\n" + os.Args[0] + " <INPUT FILENAME> [<OUTPUT FILENAME>]")
